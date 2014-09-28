70c356e0ddc9b36072f9d0b6941c74dfdef3bc91
Locke <moyerock@gmail.com>
2014-09-28 20:30:06 +0800

    add social login buttons.

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('login', {title: 'login'});
+    res.render('login', {title: '第三方帐号登录'});
 });
 
 module.exports = router;
