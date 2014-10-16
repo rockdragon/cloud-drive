1379e1c64dc4be456696a4b368980b76db7d4e5d
Locke <moyerock@gmail.com>
2014-10-16 21:59:07 +0800

    prepare file handler for uploading.

--- modules/upload/upload.js
+++ modules/upload/upload.js
@@ -1,8 +1,11 @@
 var userUtils = require('../auth/userUtils');
+var path = require('path');
 
 module.exports.bind = function (server) {
+    var Files = {};
+
     var io = require('socket.io').listen(server);
-    var respTime = function(socket){
+    var respTime = function (socket) {
         socket.send((new Date()).getTime());
     };
     io.sockets.on('connection', function (socket) {
@@ -10,20 +13,48 @@ module.exports.bind = function (server) {
 
         respTime(socket);
 
-        socket.on('message', function(message) {
+        socket.on('message', function (message) {
             console.log('received message: ' + message);
         });
 
-        socket.on('start', function(data){
-            console.log('uploading %s, size: %d, session: %s', data['Name'], data['Size'], data['SessionId']);
+        //starting upload
+        socket.on('start', function (data) {
+            var name = data.Name;
+            var size = data.Size;
+            var sessionId = data.SessionId;
+            console.log('uploading %s, size: %d, session: %s', name, size, sessionId);
 
-            userUtils.getUserRootPath(data['SessionId'], function(err, path) {
-                console.log(path);
+            //combine path
+            userUtils.getUserRootPath(sessionId, function (err, userRootPath) {
+                Files[name] = {
+                    fileSize: size,
+                    data: '',
+                    downloaded: 0,
+                    handler: null
+                };
+                var position = 0;
+                try {
+                    var filePath = path.join(userRootPath, name);
+                    console.log('sessionId: %s, uploading: %s ...', sessionId, filePath);
+                    var stat = fs.statSync(filePath);
+                    if (stat.isFile()) {
+                        Files[name].download = stat.size;
+                        position = stat.size;
+                    }
+                } catch (err) {
+                }
+                fs.open(filePath, 'a', 0755, function (err, fd) {
+                    if (err)
+                        console.log('file open error: ' + err.toString());
+                    else {
+                        Files[name].handler = fd;
+                        socket.emit('moreData', {position: position, percent: 0});
+                    }
+                });
             });
-
-            respTime(socket);
         });
-        socket.on('upload', function(data){
+        //uploading
+        socket.on('upload', function (data) {
             console.log('uploading %s, size: %d', data['Name'], data['Size']);
             respTime(socket);
         });
