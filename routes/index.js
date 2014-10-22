54138bf9bdbdadc2b130e711d5c509ce2deacbb3
rockdragon <moyerock@gmail.com>
2014-10-22 10:25:06 +0800

    add mock up storage table.

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,45 @@ var router = express.Router();
 var session = require('../modules/sessions/sessionUtils');
 var userUtils = require('../modules/auth/userUtils');
 
+//mock up
+var storage = {
+    files: [
+        {
+            name: '1.zip',
+            path: '/users/moye/1.zip',
+            size: '1.1M',
+            suffix: 'zip',
+            mime: 'zip',
+            modified: '2011/04/25 11:20AM'
+        },
+        {
+            name: '2.zip',
+            path: '/users/moye/2.zip',
+            size: '2.1M',
+            suffix: 'zip',
+            mime: 'zip',
+            modified: '2011/04/25 10:00PM'
+        }
+    ],
+    folders: [
+        {
+            name: 'home',
+            path: '/users/moye/home',
+            route: 'home',
+            folders:[
+                {
+                    name: 'second',
+                    path: '/users/moye/home/second',
+                    route: 'home/second',
+                    folders: [],
+                    files: []
+                }
+            ],
+            files: []
+        }
+    ]
+};
+
 /* GET home page. */
 router.route('/').get(function (req, res) {
     var user = null;
@@ -14,9 +53,9 @@ router.route('/').get(function (req, res) {
             user = JSON.parse(reply);
         }
         if(user)
-            res.render('users/user_index', {title: 'Welcome to cloud-drive.', user: user});
+            res.render('users/user_index', {title: 'Cloud-drive.', user: user, storage: storage});
         else
-            res.render('index', {title: 'Welcome to cloud-drive.', user: user});
+            res.render('index', {title: 'Cloud-drive.', user: user, storage: storage});
     });
 });
 
