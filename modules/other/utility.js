0be70399092715c5b7419484ae221398f3f1989d
rockdragon <moyerock@gmail.com>
2014-10-27 13:22:31 +0800

    add cross-platform folder creation logic.

--- modules/other/utility.js
+++ modules/other/utility.js
@@ -1,6 +1,20 @@
-String.prototype.contains = function(str){
-    return this.indexOf(str) > -1;
-};
+if (typeof String.prototype.contains !== 'function') {
+    String.prototype.contains = function (str) {
+        return this.indexOf(str) > -1;
+    };
+}
+
+if(typeof String.prototype.startsWith !== 'function'){
+    String.prototype.startsWith = function(str) {
+        return new RegExp('^' + str).test(this);
+    };
+}
+
+if(typeof String.prototype.endsWith !== 'function'){
+    String.prototype.endsWith = function(str) {
+        return new RegExp(str + '$').test(this);
+    };
+}
 
 module.exports.isWin = function(){
     return process.platform === 'win32';
--- test/miscellaneous/testPath.js
+++ test/miscellaneous/testPath.js
@@ -1,5 +1,12 @@
 (function () {
-    var pathUtils = require('../../modules/upload/pathUtils');
+//    var pathUtils = require('../../modules/upload/pathUtils');
+//    pathUtils.mkdirAbsoluteSync('D:\\files\\111');
 
-    pathUtils.mkdirAbsoluteSync('D:\\files\\111');
+    var utility = require('../../modules/other/utility');
+
+    console.log('hello'.startsWith('he'));
+
+    console.log('hello'.endsWith('he'));
+    console.log('hello'.endsWith('llo'));
+
 })();
